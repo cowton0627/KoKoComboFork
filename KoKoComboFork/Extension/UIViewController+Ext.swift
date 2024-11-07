0f7e2fbebb8a1a5ef5bd17b3c9b77872ddef20a0
cowton0627 <83654992+cowton0627@users.noreply.github.com>
2024-11-07 10:37:29 +0800

    set unuse page to initial ViewController, show the message "being constructed"

---
+++ KoKoComboFork/Extension/UIViewController+Ext.swift
@@ -0,0 +1,12 @@
+//
+//  UIViewController+Ext.swift
+//  KoKoComboFork
+//
+//  Created by cowton0627 on 2024/11/6.
+//
+
+import UIKit
+
+extension UIViewController {
+    static var identifier: String { return String(describing: self) }
+}
