d9e230c383cd9167f25e6bef783cca246aaa788c
cowton0627 <83654992+cowton0627@users.noreply.github.com>
2024-11-19 11:28:52 +0800

    modify custom gradient button

--- KoKoComboFork/View/Customised/Button/CustomGradientButton.swift
+++ KoKoComboFork/View/Customised/Button/CustomGradientButton.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 
+/// 客製化加好友 Button
 class CustomGradientButton: UIButton {
 
     private let buttonTitle = "加好友"
     private let icAddFriendWhite = UIImage(named: "icAddFriendWhite")
     private let gradientLayer = CAGradientLayer()
+    private let firstColor = #colorLiteral(red: 0.1019607843, green: 0.6, blue: 0.1019607843, alpha: 1).cgColor
+    private let secondColor = #colorLiteral(red: 0.2980392157, green: 0.8, blue: 0.2980392157, alpha: 1).cgColor
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -24,17 +27,17 @@ class CustomGradientButton: UIButton {
     }
     
     private func setupButton() {
-        // 設置漸層顏色
+        // 設置漸層色
         gradientLayer.colors = [
-            UIColor(red: 0.1, green: 0.6, blue: 0.1, alpha: 1).cgColor,
-            UIColor(red: 0.3, green: 0.8, blue: 0.3, alpha: 1).cgColor
+            firstColor,
+            secondColor
         ]
         gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
         gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
         gradientLayer.cornerRadius = 20
         gradientLayer.masksToBounds = true
         
-        // 添加漸層到按鈕
+        // 加上漸層
         layer.insertSublayer(gradientLayer, at: 0)
         
         
@@ -56,8 +59,8 @@ class CustomGradientButton: UIButton {
             let imageWidth = icAddFriendWhite?.size.width ?? 0
             let titlePadding = (titleImgPadding + imageWidth)
 
-            // 設置內容的內邊距
-            config.contentInsets = 
+            // 設置內邊距
+            config.contentInsets =
             NSDirectionalEdgeInsets(top: 10, 
                                     leading: 8 + titlePadding,
                                     bottom: 10,
