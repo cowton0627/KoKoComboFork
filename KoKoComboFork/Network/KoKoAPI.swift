d0a3e07a96bc30ba97d9f10c316eebff3536331d
cowton0627 <83654992+cowton0627@users.noreply.github.com>
2024-11-19 11:24:22 +0800

    modify api and userservice

--- KoKoComboFork/Network/KoKoAPI.swift
+++ KoKoComboFork/Network/KoKoAPI.swift
@@ -16,17 +16,17 @@ struct KoKoAPI {
     /// - 2：好友列表二，status 僅有 1
     /// - 3：好友列表三，status 有 0、1、2
     /// - 4：好友列表四，空
-    static var scenario = 1
+//    static var scenario = 1
     
     enum Endpoint {
         case getUserData
-        case getFriendsData
+        case getFriendsData(scenario: Int)
         
         var urlString: String {
             switch self {
             case .getUserData:
                 return "\(KoKoAPI.rootEndpoint)man.json"
-            case .getFriendsData:
+            case .getFriendsData(let scenario) :
                 return "\(KoKoAPI.rootEndpoint)friend\(scenario).json"
             }
         }
