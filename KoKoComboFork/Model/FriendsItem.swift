6ba251a98eb4b423f61157617c7171a186d6dd52
Charles Cheng <charles_cheng@microjet.com.tw>
2024-11-19 11:22:00 +0800

    add friendsitem, but not use it

---
+++ KoKoComboFork/Model/FriendsItem.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsItem.swift
+//  KoKoComboFork
+//
+//  Created by Chun-Li Cheng on 2024/11/18.
+//
+
+import Foundation
+
+// 若有特殊需求才設置另一個 Model，目前先用 Friend
+struct FriendsItem {
+    let name: String
+    let status: Int
+    let isTop: String
+//    let fid: String
+//    let updateDate: String
+}
